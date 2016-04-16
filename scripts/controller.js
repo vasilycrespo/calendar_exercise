b1d53227abd3a4cc7b74f3987f65bc6bb8e704db
root <root@localhost.localdomain>
2016-04-15 19:56:36 -0430

    Captured user information to be used on further functions

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,4 +1,45 @@
 var app = angular.module('myCalendar', []);
 app.controller("calendarCtrl", function($scope, $timeout, $interval, $animate, $http){
-	console.log("hello world");
+	
+	//User information
+	$scope.inputDate = null;
+	$scope.inputDays = null;
+	$scope.inputCountry = null;
+
+	$scope.generateCalendar = function(){
+	
+		//Some Validations
+
+		//Is a proper date
+		var isDate = /^[\d]{1,2}\/[\d]{1,2}\/[\d]{4}$/ig.test($scope.inputDate);
+		if(isDate){
+			var group = /^([\d]{1,2})\/([\d]{1,2})\/([\d]{4})$/ig.exec($scope.inputDate);
+			$scope.userDate = {"d":group[2],"m":group[1],"y":group[3]};
+			$scope.startDate = new Date($scope.userDate.y,$scope.userDate.m,$scope.userDate.d);
+			$scope.endDate = null;
+			$scope.currentMonth = null;
+		}else { 
+			alert("The date is not valid");
+			return false;
+		};
+
+		//Is a proper number for days
+		var isNumber = /^[\d]+$/ig.test($scope.inputDays);
+		if(isNumber){
+			$scope.numberOfDays = parseInt($scope.inputDays);
+		}else { 
+			alert("The provided days is not valid");
+			return false;
+		};
+
+		//Is a proper country code
+		var isCountrycode = /^[A-Za-z]{2}$/ig.test($scope.inputCountry);
+		if(!isCountrycode){
+			alert("The provided country code is not valid");
+			return false;
+		}
+
+
+	};
+
 });
